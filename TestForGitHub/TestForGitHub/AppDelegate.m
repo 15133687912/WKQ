7af3268bd3c3f939f862c9f04f5889ee353006b1
吴凯强 <370006600@qq.com>
2016-01-14 11:13:55 +0800

    6.0

--- TestForGitHub/TestForGitHub/AppDelegate.m
+++ TestForGitHub/TestForGitHub/AppDelegate.m
@@ -13,6 +13,8 @@
 @property(strong,nonatomic)UILabel *myLabel;
 @property(strong,nonatomic)UIButton *myButton;
 @property(strong,nonatomic)UILabel *label;
+@property(strong,nonatomic)UILabel *label1;
+
 @end
 
 @implementation AppDelegate
