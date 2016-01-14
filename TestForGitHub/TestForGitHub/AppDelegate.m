f273a47427652170ad824d5c66eca1fc1f87a508
吴凯强 <370006600@qq.com>
2016-01-14 10:16:41 +0800

    版本2.0

--- TestForGitHub/TestForGitHub/AppDelegate.m
+++ TestForGitHub/TestForGitHub/AppDelegate.m
@@ -10,12 +10,19 @@
 
 @interface AppDelegate ()
 
+@property(strong,nonatomic)UILabel *myLabel;
+
 @end
 
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    
+    
+    
+    
     // Override point for customization after application launch.
     return YES;
 }
